d2abfff072e20b8048534da1d10b98ea6463c903
Vatsal Amin <aminvatsal1999@gmail.com>
2023-12-29 05:54:30 +0000

    Feature/buttons 1

--- include/Button.h
+++ include/Button.h
@@ -1,23 +1,29 @@
 #pragma once
 
-#include "raylib.h"
+#include <math.h>
+#include <raylib.h>
+#include <string>
+#include "Text.h"
 
 namespace tv
 {
-   class Button
-   {
-   public:
+  class Button
+  {
+    public:
       /// @brief Constructor
-      /// @param position: Rectangle position
-      /// @param size: Rectangle size
-      /// @param color: Rectangle color
-      Button(Vector2 position, Vector2 size, Color color)
-      :  size(size),
-         color(color),
-         position(Vector2(position.x - (size.x / 2), position.y - (size.y / 2)))
+      /// @param position: Button position
+      /// @param text: Button text
+      /// @param bSize: Button size
+      /// @param color: Button color
+      Button(Vector2 position, std::string txt, int bSize, Color color)
+      : color(color)
       {
+        text = Text(txt, position, bSize);
+        this->size = Vector2(text.textSize.x + constants::TEXT_BUTTON_SPACING * 2, text.textSize.y + constants::TEXT_BUTTON_SPACING);
+        this->position = Vector2(position.x - constants::TEXT_BUTTON_SPACING - text.textSize.x/2, 
+            position.y - constants::TEXT_BUTTON_SPACING/2 - text.textSize.y/2);
       }
-      
+
       /// @brief Delete default constructor
       Button() = delete;
 
@@ -27,19 +33,24 @@ namespace tv
       /// @brief Delete move constructor
       Button(Button&&) = delete;
 
+      /// @brief Draw the button
       void Draw()
       {
-         DrawRectangleV(position, size, color);
+        DrawRectangleV(position, size, color);
+        text.Draw();
       }
 
-      /// @brief Rectangle size
-      Vector2 size{};
-      
-      /// @brief Rectangle color
+      /// @brief Button color
       Color color{};
 
-      /// @brief Rectangle position
+      /// @brief Button size
+      Vector2 size{};
+
+      /// @brief Button position
       Vector2 position{};
-   };
+
+      /// @brief Button text
+      Text text{};
+  };
 
 } // namespace tv
